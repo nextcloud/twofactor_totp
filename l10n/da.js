9907b9af8b0fef769d0fc62a25767fc0e89324b4
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-07-01 00:41:34 -0400

    [tx-robot] updated from transifex

---
+++ l10n/da.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "twofactor_totp",
+    {
+    "Enable TOTP" : "Aktivér TOTP",
+    "This is your new TOTP secret:" : "Dette er din nye hemmelige TOTP :",
+    "Scan this QR code with your TOTP app" : "Scan denne QR-kode med din TOTP app",
+    "Authenticate with a TOTP app" : "Godkend med en TOTP app",
+    "TOTP Second-factor Auth" : "TOTP To-faktor godkendelse"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/da.php
@@ -0,0 +1,9 @@
+<?php
+$TRANSLATIONS = array(
+"Enable TOTP" => "Aktivér TOTP",
+"This is your new TOTP secret:" => "Dette er din nye hemmelige TOTP :",
+"Scan this QR code with your TOTP app" => "Scan denne QR-kode med din TOTP app",
+"Authenticate with a TOTP app" => "Godkend med en TOTP app",
+"TOTP Second-factor Auth" => "TOTP To-faktor godkendelse"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
