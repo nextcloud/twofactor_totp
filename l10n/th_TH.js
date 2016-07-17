e0246fb8832521e9afb885e4dd5f049cf8c21989
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-07-17 00:40:27 -0400

    [tx-robot] updated from transifex

--- l10n/th_TH.js
+++ l10n/th_TH.js
@@ -5,6 +5,6 @@ OC.L10N.register(
     "This is your new TOTP secret:" : "นี่คือข้อความลับ TOTP ใหม่ของคุณ:",
     "Scan this QR code with your TOTP app" : "สแกน QR โค้ดนี้กับแอพฯ TOTP ของคุณ",
     "Authenticate with a TOTP app" : "ตรวจสอบสิทธิ์ด้วยแอพฯ TOTP",
-    "TOTP Second-factor Auth" : "TOTP รับรองความถูกต้องสองชั้น"
+    "TOTP Second-factor Auth" : "TOTP รับรองความถูกต้องขั้นตอนที่สอง"
 },
 "nplurals=1; plural=0;");
