8af390d767c12629327259726ac5f8b4d8bc18be
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-08-02 18:40:56 -0400

    [tx-robot] updated from transifex

--- l10n/tr.js
+++ l10n/tr.js
@@ -3,8 +3,8 @@ OC.L10N.register(
     {
     "Enable TOTP" : "TOTP Etkinleştir",
     "This is your new TOTP secret:" : "Yeni TOTP sırrınız:",
-    "Scan this QR code with your TOTP app" : "Bu QR kodu TOTP uygulamanızla tarayın",
-    "Authenticate with a TOTP app" : "Bir TOTP uygulamasıyla kimlik kanıtlaması yapın",
-    "TOTP Second-factor Auth" : "TOTP İkinci faktör Kanıtlaması"
+    "Scan this QR code with your TOTP app" : "Bu QR kodunu TOTP uygulamanızla tarayın",
+    "Authenticate with a TOTP app" : "Bir TOTP uygulamasıyla kimlik doğrulaması yapın",
+    "TOTP Second-factor Auth" : "TOTP İkinci Yöntem Doğrulaması"
 },
 "nplurals=2; plural=(n > 1);");
--- l10n/tr.php
+++ l10n/tr.php
@@ -2,8 +2,8 @@
 $TRANSLATIONS = array(
 "Enable TOTP" => "TOTP Etkinleştir",
 "This is your new TOTP secret:" => "Yeni TOTP sırrınız:",
-"Scan this QR code with your TOTP app" => "Bu QR kodu TOTP uygulamanızla tarayın",
-"Authenticate with a TOTP app" => "Bir TOTP uygulamasıyla kimlik kanıtlaması yapın",
-"TOTP Second-factor Auth" => "TOTP İkinci faktör Kanıtlaması"
+"Scan this QR code with your TOTP app" => "Bu QR kodunu TOTP uygulamanızla tarayın",
+"Authenticate with a TOTP app" => "Bir TOTP uygulamasıyla kimlik doğrulaması yapın",
+"TOTP Second-factor Auth" => "TOTP İkinci Yöntem Doğrulaması"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
