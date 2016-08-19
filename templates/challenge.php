f36a3c98b4d6834518d4ea2fd407d8a6c895a090
Loki3000 <github@labcms.ru>
2016-08-19 10:40:16 +0300

    Update challenge.php

--- templates/challenge.php
+++ templates/challenge.php
@@ -1,4 +1,4 @@
 <form method="POST">
-    <input type="text" name="challenge" required="required" autocomplete="off" autocapitalize="off">
+    <input type="text" name="challenge" required="required" autofocus autocomplete="off" autocapitalize="off">
     <input type="submit" class="button" value="Verify">
 </form>
