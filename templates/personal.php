1eeea46b067db6d28d90a2e53fecd75b1d454814
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-08-28 00:03:42 +0200

    fix name capitalization

--- templates/personal.php
+++ templates/personal.php
@@ -6,6 +6,6 @@ script('twofactor_totp', 'settings');
 ?>
 
 <div class="section">
-    <h2><?php p($l->t('TOTP Second-factor Auth')); ?></h2>
+    <h2><?php p($l->t('TOTP second-factor auth')); ?></h2>
     <div id="twofactor-totp-settings"></div>
 </div>
