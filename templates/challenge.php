f8cce1f5a1078b1726266f21fa861c622f556ecf
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-08-27 12:33:49 +0200

    enhanced challenge template styling

--- templates/challenge.php
+++ templates/challenge.php
@@ -1,4 +1,9 @@
-<form method="POST">
-    <input type="text" name="challenge" required="required" autofocus autocomplete="off" autocapitalize="off">
-    <input type="submit" class="button" value="Verify">
+<?php
+style('twofactor_totp', 'style');
+?>
+
+<form method="POST" class="totp-form">
+	<input type="text" class="challenge" name="challenge" required="required" autofocus autocomplete="off" autocapitalize="off" placeholder="<?php p($l->t('Authentication code')) ?>">
+	<input type="submit" class="confirm-inline icon-confirm" value="">
+	<p>Get the authentication code from the two-factor authentication app on your device.</p>
 </form>
