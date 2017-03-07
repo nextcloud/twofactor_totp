f22abdd065e436affe96313786bbacd721358bb5
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-03-08 00:43:38 +0100

    [tx-robot] updated from transifex

---
+++ l10n/is.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "twofactor_totp",
+    {
+    "Enable TOTP" : "Virkja TOTP",
+    "This is your new TOTP secret:" : "Þetta er nýi TOTP-leynilykillinn þinn:",
+    "Scan this QR code with your TOTP app" : "Skannaðu þennan QR-kóða með TOTP-forritinu þínu",
+    "Authenticate with a TOTP app" : "Auðkenna með TOTP-forriti",
+    "TOTP Second-factor Auth" : "TOTP tveggja-skrefa auðkenning"
+},
+"nplurals=2; plural=(n % 10 != 1 || n % 100 == 11);");
---
+++ l10n/ko.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "twofactor_totp",
+    {
+    "Enable TOTP" : "TOTP 사용",
+    "This is your new TOTP secret:" : "새로운 TOTP 비밀 키 값입니다:",
+    "Scan this QR code with your TOTP app" : "다음 QR 코드를 TOTP 앱으로 스캔하십시오.",
+    "Authenticate with a TOTP app" : "TOTP 앱으로 인증",
+    "TOTP Second-factor Auth" : "TOTP 2단계 인증"
+},
+"nplurals=1; plural=0;");
