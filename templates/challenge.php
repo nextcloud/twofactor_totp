e30dd079d2008cead24fc9fa87ff83dd934b1c04
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-03-07 08:50:42 +0100

    Use dedicated login button on 2fa challenge page
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- templates/challenge.php
+++ templates/challenge.php
@@ -3,7 +3,9 @@ style('twofactor_totp', 'style');
 ?>
 
 <form method="POST" class="totp-form">
-	<input type="text" class="challenge" name="challenge" required="required" autofocus autocomplete="off" autocapitalize="off" placeholder="<?php p($l->t('Authentication code')) ?>">
-	<input type="submit" class="confirm-inline icon-confirm" value="">
+	<input type="tel" minlength="6" maxlength="6" name="challenge" required="required" autofocus autocomplete="off" autocapitalize="off" placeholder="<?php p($l->t('Authentication code')) ?>">
+	<button type="submit">
+		<span>Submit</span>
+	</button>
 	<p><?php p($l->t('Get the authentication code from the two-factor authentication app on your device.')) ?></p>
 </form>
