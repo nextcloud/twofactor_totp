ee7fb3567f03b2730ddd3029dfefc5de87924e21
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-03-09 18:43:47 +0100

    Make the submit button translatable
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- templates/challenge.php
+++ templates/challenge.php
@@ -5,7 +5,7 @@ style('twofactor_totp', 'style');
 <form method="POST" class="totp-form">
 	<input type="tel" minlength="6" maxlength="6" name="challenge" required="required" autofocus autocomplete="off" autocapitalize="off" placeholder="<?php p($l->t('Authentication code')) ?>">
 	<button type="submit">
-		<span>Submit</span>
+		<span><?php p($l->t('Submit')); ?></span>
 	</button>
 	<p><?php p($l->t('Get the authentication code from the two-factor authentication app on your device.')) ?></p>
 </form>
