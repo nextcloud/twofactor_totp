6a71dc6ddf6cf77e6e5d4da6c9efdb9c2721d38d
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-04-11 00:50:04 +0200

    [tx-robot] updated from transifex

--- l10n/pt_PT.js
+++ l10n/pt_PT.js
@@ -5,6 +5,6 @@ OC.L10N.register(
     "This is your new TOTP secret:" : "Este é o seu novo segredo TOTP:",
     "Scan this QR code with your TOTP app" : "Leia-a este código QR com a sua app TOTP",
     "Authenticate with a TOTP app" : "Autenticação com uma app TOTP",
-    "TOTP Second-factor Auth" : "Autenticação TOTP de Dois Fatores"
+    "TOTP Second-factor Auth" : "Autenticação de Dois Fatores TOTP"
 },
 "nplurals=2; plural=(n != 1);");
--- l10n/sl.js
+++ l10n/sl.js
@@ -5,6 +5,6 @@ OC.L10N.register(
     "This is your new TOTP secret:" : "Nastavljeno je novo enkratno geslo:",
     "Scan this QR code with your TOTP app" : "Preberi kodo QR enkratnega gesla z ustreznim programom",
     "Authenticate with a TOTP app" : "Overi z enkratnim geslom",
-    "TOTP Second-factor Auth" : "Overitev z enkratnim geslom"
+    "TOTP Second-factor Auth" : "Overitev z dvojnim enkratnim geslom"
 },
 "nplurals=4; plural=(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3);");
