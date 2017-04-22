8e70c84e8136dec5d8dfbf71388bf54544cf8f87
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-04-23 00:40:16 +0200

    [tx-robot] updated from transifex

--- l10n/el.js
+++ l10n/el.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Enable TOTP" : "Ενεργοποίηση TOTP",
     "This is your new TOTP secret:" : "Αυτό είναι το νέο απόκρυφο ΤΟΤΡ ",
     "Scan this QR code with your TOTP app" : "Σάρωση αυτού του κώδικα QR με την εφαρμογή ΤΟΤΡ",
-    "Authenticate with a TOTP app" : "Πιστοποίηση με την εφαρμογή ΤΟΤΡ"
+    "Authenticate with a TOTP app" : "Πιστοποίηση με την εφαρμογή ΤΟΤΡ",
+    "TOTP Second-factor Auth" : "TOTP Δευτερεύον - παράγοντας Auth"
 },
 "nplurals=2; plural=(n != 1);");
