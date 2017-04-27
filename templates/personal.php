11f34786540e0234ff9b048e064c4d8db74878b3
Morris Jobke <hey@morrisjobke.de>
2017-04-27 18:05:41 -0300

    Fix icons in sidebar on personal page
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- templates/personal.php
+++ templates/personal.php
@@ -7,6 +7,6 @@ style('twofactor_totp', 'settings');
 ?>
 
 <div class="section">
-    <h2><?php p($l->t('TOTP second-factor auth')); ?></h2>
+    <h2 data-anchor-name="totp-second-factor-auth"><?php p($l->t('TOTP second-factor auth')); ?></h2>
     <div id="twofactor-totp-settings"></div>
 </div>
