9a1e09d2aac4e60bddf94c1c89c0f638f76353cd
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-05-02 00:40:16 +0200

    [tx-robot] updated from transifex

--- l10n/ko.js
+++ l10n/ko.js
@@ -3,7 +3,7 @@ OC.L10N.register(
     {
     "Enable TOTP" : "TOTP 사용",
     "This is your new TOTP secret:" : "새로운 TOTP 비밀 키 값입니다:",
-    "Scan this QR code with your TOTP app" : "다음 QR 코드를 TOTP 앱으로 스캔하십시오.",
+    "Scan this QR code with your TOTP app" : "다음 QR 코드를 TOTP 앱으로 스캔하십시오",
     "Authenticate with a TOTP app" : "TOTP 앱으로 인증",
     "TOTP Second-factor Auth" : "TOTP 2단계 인증"
 },
