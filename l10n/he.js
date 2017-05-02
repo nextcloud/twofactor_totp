a5c360b9386806b1ff7a42e3682c551bda6670a0
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-05-02 21:33:40 +0200

    [tx-robot] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -1,8 +1,14 @@
 OC.L10N.register(
     "twofactor_totp",
     {
+    "Activate TOTP" : "הפעלת TOTP",
     "This is your new TOTP secret:" : "להלן סיסמת ה- TOTP החדשה שלך:",
     "Scan this QR code with your TOTP app" : "יש לסרוק את קוד ה- QR עם יישום ה- TOTP שלך",
+    "You can test a generated authentication code below." : "ניתן לבדוק מטה קוד אימות שהופק.",
+    "Authentication code" : "קוד אימות",
+    "Verify" : "אימות",
+    "Verified" : "מאומת",
+    "Not verified" : "לא מאומת",
     "Authenticate with a TOTP app" : "אימות עם יישום ה- TOTP",
     "TOTP Second-factor Auth" : "אימות מקדם שני של TOTP"
 },
--- l10n/pt_PT.js
+++ l10n/pt_PT.js
@@ -1,8 +1,14 @@
 OC.L10N.register(
     "twofactor_totp",
     {
+    "Activate TOTP" : "Ativar TOTP",
     "This is your new TOTP secret:" : "Este é o seu novo segredo TOTP:",
     "Scan this QR code with your TOTP app" : "Leia-a este código QR com a sua app TOTP",
+    "You can test a generated authentication code below." : "Pode testar abaixo a geração de um código de autenticação.",
+    "Authentication code" : "Código de autenticação",
+    "Verify" : "Verificar",
+    "Verified" : "Verificado",
+    "Not verified" : "Não verificado",
     "Authenticate with a TOTP app" : "Autenticação com uma app TOTP",
     "TOTP Second-factor Auth" : "Autenticação de Dois Fatores TOTP"
 },
