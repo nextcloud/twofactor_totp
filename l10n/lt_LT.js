9fc89051bc0adae96bbce39e36cb249aa024910c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-05-12 00:40:14 +0200

    [tx-robot] updated from transifex

--- l10n/lt_LT.js
+++ l10n/lt_LT.js
@@ -1,8 +1,14 @@
 OC.L10N.register(
     "twofactor_totp",
     {
+    "Activate TOTP" : "Aktyvuoti TOTP",
     "This is your new TOTP secret:" : "Jūsų naujasis TOTP raktas:",
     "Scan this QR code with your TOTP app" : "Nuskanuokite šį QR kodą su savo TOTP programėle",
+    "You can test a generated authentication code below." : "Žemiau galite išbandyti sukurtą autentifikacijos kodą.",
+    "Authentication code" : "Autentifikacijos kodas",
+    "Verify" : "Patvirtinti",
+    "Verified" : "Patvirtinta",
+    "Not verified" : "Nepatvirtinta",
     "Authenticate with a TOTP app" : "Prisijungti su TOTP programėle",
     "TOTP Second-factor Auth" : "TOTP papildoma autorizacija"
 },
