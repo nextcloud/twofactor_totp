74f4ab17f2d1f354308cff5b89fdff9eef05f959
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-05-23 00:42:27 +0200

    [tx-robot] updated from transifex

--- l10n/ja.js
+++ l10n/ja.js
@@ -1,6 +1,7 @@
 OC.L10N.register(
     "twofactor_totp",
     {
+    "Activate TOTP" : "アクティブTOTP",
     "This is your new TOTP secret:" : "TOTPの新しい秘密鍵",
     "Scan this QR code with your TOTP app" : "TOTPアプリでQRコードをスキャンしてください",
     "Authentication code" : "認証コード",
