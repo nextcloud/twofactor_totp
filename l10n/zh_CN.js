c99d9917704c4835a72a2ca622f4b210688f9e18
ownCloud Jenkins <jenkins@owncloud.com>
2017-07-21 01:27:14 +0000

    [tx-robot] updated from transifex

---
+++ l10n/zh_CN.js
@@ -0,0 +1,15 @@
+OC.L10N.register(
+    "twofactor_totp",
+    {
+    "Activate TOTP" : "激活 TOTP",
+    "This is your new TOTP secret:" : "这是您新的 TOTP 密文：",
+    "Scan this QR code with your TOTP app" : "使用您的 TOTP 应用扫描该二维码",
+    "You can test a generated authentication code below." : "你可以在下方测试生成的验证码。",
+    "Authentication code" : "验证码",
+    "Verify" : "验证",
+    "Verified" : "已验证",
+    "Not verified" : "未验证",
+    "Authenticate with a TOTP app" : "使用 TOTP 应用进行验证",
+    "TOTP Second-factor Auth" : "TOTP 双因素验证"
+},
+"nplurals=1; plural=0;");
