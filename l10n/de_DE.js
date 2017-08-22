d434a00dc8eb8771746bc8ce18b3247651f70d92
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-22 01:27:14 +0000

    [tx-robot] updated from transifex

--- l10n/de_DE.js
+++ l10n/de_DE.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Activate TOTP" : "TOTP (zeitgesteuertes Einmalpasswort) aktivieren",
     "This is your new TOTP secret:" : "Dies ist Ihr neuer TOTP-Schlüssel:",
     "Scan this QR code with your TOTP app" : "Scannen Sie diesen QR-Code mit Ihrer TOTP-App",
+    "To enable second-factor verify authentication code below." : "Zur Aktivierung des zweiten Faktors, bestätigen Sie den unten aufgeführten Authentifizierungscode.",
     "Authentication code" : "Authentifizierungscode",
     "Verify" : "Überprüfen",
     "Verified" : "Geprüft",
