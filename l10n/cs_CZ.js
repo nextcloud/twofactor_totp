c01f7d52cc58b8569d0f8569be6c8a4db5e07a51
ownCloud Jenkins <jenkins@owncloud.com>
2017-10-04 01:27:16 +0000

    [tx-robot] updated from transifex

--- l10n/cs_CZ.js
+++ l10n/cs_CZ.js
@@ -3,6 +3,10 @@ OC.L10N.register(
     {
     "This is your new TOTP secret:" : "Toto je vaše nové TOTP tajemství:",
     "Scan this QR code with your TOTP app" : "Naskenujte tento QR kód svou TOTP aplikací",
+    "Authentication code" : "Ověřovací kód",
+    "Verify" : "Ověřit",
+    "Verified" : "Ověřeno",
+    "Not verified" : "Neověřeno",
     "Authenticate with a TOTP app" : "Přihlaste se pomocí TOTP aplikace",
     "TOTP Second-factor Auth" : "TOTP ověření druhým faktorem"
 },
