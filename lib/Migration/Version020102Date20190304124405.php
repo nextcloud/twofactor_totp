9c501a5c4dc8639b56b780803adc67b94b77ecd6
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-03-04 13:21:20 +0100

    Fix first migration from an old database schema
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ lib/Migration/Version020102Date20190304124405.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\TwoFactorTOTP\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+
+class Version020102Date20190304124405 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 *
+	 * @return ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		$table = $schema->getTable('twofactor_totp_secrets');
+		if (!$table->hasColumn('state')) {
+			$table->addColumn('state', 'integer', [
+				'notnull' => true,
+				'default' => 2,
+			]);
+		}
+		if (!$table->hasPrimaryKey()) {
+			$table->setPrimaryKey(['id']);
+		}
+		if (!$table->hasIndex('totp_secrets_user_id')) {
+			$table->addUniqueIndex(['user_id'], 'totp_secrets_user_id');
+		}
+
+		return $schema;
+	}
+
+}
