8b0ed095fb57f9bbf10bb73b4c4dc3759aebb1f6
ownClouders <devops@owncloud.com>
2019-08-31 01:19:52 +0000

    [tx] updated from transifex

---
+++ l10n/eu.js
@@ -0,0 +1,15 @@
+OC.L10N.register(
+    "twofactor_totp",
+    {
+    "Activate TOTP" : "TOTP aktibatu",
+    "This is your new TOTP secret:" : "Hau da zure TOTP isileko berria:",
+    "Scan this QR code with your TOTP app" : "Eskaneatu QR kode hau zure TOTP aplikazioarekin",
+    "To enable second-factor verify authentication code below." : "Bigarren-faktorea gaitzeko, egiaztatu azpiko autentifikazio kodea.",
+    "Authentication code" : "Autentifikazioa kodea",
+    "Verify" : "Egiaztatu",
+    "Verified" : "Egiaztatua",
+    "Not verified" : "Egiaztatu gabea",
+    "Authenticate with a TOTP app" : "Egiaztatu TOTP aplikazioarekin",
+    "TOTP Second-factor Auth" : "TOTP Bigarren-faktore Autentifikazioa"
+},
+"nplurals=2; plural=(n != 1);");
