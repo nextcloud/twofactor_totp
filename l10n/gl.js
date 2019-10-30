9d12cbbd0e91beb6324d536962b0969a1b0e2957
ownClouders <devops@owncloud.com>
2019-10-30 12:51:11 +0000

    [tx] updated from transifex

--- l10n/gl.js
+++ l10n/gl.js
@@ -3,7 +3,7 @@ OC.L10N.register(
     {
     "Activate TOTP" : "Activar TOTP",
     "This is your new TOTP secret:" : "Esta é a súa nova TOTP secreta:",
-    "Scan this QR code with your TOTP app" : "Lea este código QR co seu aplicativo TOTP",
+    "Scan this QR code with your TOTP app" : "Lea este código QR coa súa aplicación TOTP",
     "To enable second-factor verify authentication code below." : "Para activar o segundo factor, verifique o código de autenticación a continuación.",
     "Authentication code" : "Código de autenticación",
     "Verify" : "Verificar",
--- l10n/th_TH.js
+++ l10n/th_TH.js
@@ -1,15 +1,15 @@
 OC.L10N.register(
     "twofactor_totp",
     {
-    "Activate TOTP" : "เปิดใช้งาน TOTP",
+    "Activate TOTP" : "เปิดใช้งาน TOTP (Time-Based One-Time Password Algorithm)",
     "This is your new TOTP secret:" : "นี่คือข้อความลับ TOTP ใหม่ของคุณ:",
-    "Scan this QR code with your TOTP app" : "สแกน QR โค้ดนี้กับแอพฯ TOTP ของคุณ",
-    "To enable second-factor verify authentication code below." : "เพื่อเปิดใช้งานการตรวจสอบสิทธิ์ของรหัสที่สองได้ที่ด้านล่าง",
+    "Scan this QR code with your TOTP app" : "สแกน QR โค้ดนี้กับแอปฯ TOTP ของคุณ",
+    "To enable second-factor verify authentication code below." : "เพื่อเปิดใช้งานกรุณากรอกรหัสได้ที่ด้านล่าง",
     "Authentication code" : "รหัสรับรองความถูกต้อง",
     "Verify" : "ยืนยัน",
     "Verified" : "ยืนยันแล้ว",
     "Not verified" : "ยังไม่ได้ยืนยัน",
-    "Authenticate with a TOTP app" : "ตรวจสอบสิทธิ์ด้วยแอพฯ TOTP",
-    "TOTP Second-factor Auth" : "TOTP รับรองความถูกต้องขั้นตอนที่สอง"
+    "Authenticate with a TOTP app" : "ตรวจสอบสิทธิ์ด้วยแอปฯ TOTP",
+    "TOTP Second-factor Auth" : "TOTP การยืนยันสองขั้นตอน"
 },
 "nplurals=1; plural=0;");
