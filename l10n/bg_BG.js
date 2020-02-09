ff99cc903ac2c736870fe54c0fad39177367685d
Dominik Schmidt <domme@tomahawk-player.org>
2020-02-09 02:27:57 +0000

    [tx] updated from transifex

--- l10n/bg_BG.js
+++ l10n/bg_BG.js
@@ -1,9 +1,15 @@
 OC.L10N.register(
     "twofactor_totp",
     {
+    "Activate TOTP" : "Активиране TOTP",
     "This is your new TOTP secret:" : "Това е Вашата TOTP парола:",
-    "Scan this QR code with your TOTP app" : "Сканирайте този QR кода с Вашето TOTP приложение",
+    "Scan this QR code with your TOTP app" : "Сканирайте този QR код с Вашето TOTP приложение",
+    "To enable second-factor verify authentication code below." : "За да активирате втори фактор за удостоверяване, кода е по-долу.",
+    "Authentication code" : "Удостоверителен код",
+    "Verify" : "Потвърди",
+    "Verified" : "Потвърдено",
+    "Not verified" : "Не е потвърдено",
     "Authenticate with a TOTP app" : "Удостоверете с TOTP приложение",
-    "TOTP Second-factor Auth" : "TOTP Втори факвот за Идентифициране"
+    "TOTP Second-factor Auth" : "TOTP Втори-фактор Auth"
 },
 "nplurals=2; plural=(n != 1);");
