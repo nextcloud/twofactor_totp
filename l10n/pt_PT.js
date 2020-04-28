8ebc9113a0319fe2d3421d493368a287446d9ffb
Thomas Müller <thomas.mueller@tmit.eu>
2020-04-28 03:16:27 +0000

    [tx] updated from transifex

--- l10n/pt_PT.js
+++ l10n/pt_PT.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Activate TOTP" : "Ativar TOTP",
     "This is your new TOTP secret:" : "Este é o seu novo segredo TOTP:",
     "Scan this QR code with your TOTP app" : "Leia-a este código QR com a sua app TOTP",
+    "To enable second-factor verify authentication code below." : "Para ativar segundo-fator verifique o código de autenticação abaixo.",
     "Authentication code" : "Código de autenticação",
     "Verify" : "Verificar",
     "Verified" : "Verificado",
