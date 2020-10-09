b06995260ece0ae1d20a8da863186d6ca656b465
Thomas Müller <thomas.mueller@tmit.eu>
2020-10-09 00:05:11 +0000

    [tx] updated from transifex

--- l10n/zh_TW.js
+++ l10n/zh_TW.js
@@ -8,6 +8,7 @@ OC.L10N.register(
     "Verify" : "驗證",
     "Verified" : "已驗證",
     "Not verified" : "未驗證",
-    "Authenticate with a TOTP app" : "使用與TOTP軟體進行身份驗證"
+    "Authenticate with a TOTP app" : "使用與TOTP軟體進行身份驗證",
+    "TOTP Second-factor Auth" : "TOTP二級驗證"
 },
 "nplurals=1; plural=0;");
