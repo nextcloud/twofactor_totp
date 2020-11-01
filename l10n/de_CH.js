6e9f0295e59cc1eb3760b77bbd277b4c366739f9
Thomas Müller <thomas.mueller@tmit.eu>
2020-11-01 00:05:53 +0000

    [tx] updated from transifex

---
+++ l10n/de_CH.js
@@ -0,0 +1,15 @@
+OC.L10N.register(
+    "twofactor_totp",
+    {
+    "Activate TOTP" : "TOTP (zeitgesteuertes Einmalpasswort) aktivieren",
+    "This is your new TOTP secret:" : "Dies ist dein neuer TOTP-Schlüssel:",
+    "Scan this QR code with your TOTP app" : "Scanne diesen QR-Code mit deiner TOTP-App",
+    "To enable second-factor verify authentication code below." : "Zur Aktivierung des zweiten Faktors, bestätige den unten aufgeführten Authentifizierungscode.",
+    "Authentication code" : "Authentifizierungscode",
+    "Verify" : "Bestätigen",
+    "Verified" : "Bestätigt",
+    "Not verified" : "Nicht bestätigt",
+    "Authenticate with a TOTP app" : "Mit einer TOTP-App (zeitgesteuertes Einmalpasswort) authentifizieren",
+    "TOTP Second-factor Auth" : "TOTP-Zweifaktorauthentifizierung"
+},
+"nplurals=2; plural=(n != 1);");
