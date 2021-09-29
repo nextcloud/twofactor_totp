3517ded3b58a88de5b8099dbd96c13c9b2aa78e0
Lukas Hirt <lhirt@owncloud.com>
2021-09-29 00:47:29 +0000

    [tx] updated from transifex

--- l10n/el.js
+++ l10n/el.js
@@ -2,13 +2,14 @@ OC.L10N.register(
     "twofactor_totp",
     {
     "Activate TOTP" : "Ενεργοποίηση TOTP",
-    "This is your new TOTP secret:" : "Αυτό είναι το νέο απόκρυφο ΤΟΤΡ ",
+    "This is your new TOTP secret:" : "Αυτό είναι το νέο μυστικό ΤΟΤΡ:",
     "Scan this QR code with your TOTP app" : "Σάρωση αυτού του κώδικα QR με την εφαρμογή ΤΟΤΡ",
-    "To enable second-factor verify authentication code below." : "Για να ενεργοποιήσετε τον δεύτερο παράγοντα επαληθεύστε την ταυτότητα του κώδικα παρακάτω",
+    "To enable second-factor verify authentication code below." : "Για να ενεργοποιήσετε τον δευτερεύον παράγοντα επαληθεύστε παρακάτω τον κωδικό πιστοποίησης",
     "Authentication code" : "Κωδικός πιστοποίησης",
     "Verify" : "Επαλήθευση",
     "Verified" : "Επαληθεύτηκε",
-    "Not verified" : "Δεν επαληθεύεται",
+    "Not verified" : "Δεν επαληθεύτηκε",
+    "Time-based One-time Password" : "Μοναδικό συνθηματικό βάσει χρόνου",
     "Authenticate with a TOTP app" : "Πιστοποίηση με την εφαρμογή ΤΟΤΡ",
     "TOTP Second-factor Auth" : "TOTP Δευτερεύον - παράγοντας Auth"
 },
