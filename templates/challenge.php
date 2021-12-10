9aab976350f3a9ff2c3a4941db727a233ead4e80
Jannik Stehle <jannik.stehle@gmail.com>
2021-12-10 13:56:57 +0100

    Align the code input field with the submit button on login

--- templates/challenge.php
+++ templates/challenge.php
@@ -3,7 +3,9 @@ script('core', 'login');
 ?>
 
 <form method="POST" name="login">
-    <input type="text" name="challenge" required="required" autofocus autocomplete="off" autocapitalize="off">
+	<div class="grouptop">
+		<input type="text" name="challenge" required="required" autofocus autocomplete="off" autocapitalize="off">
+	</div>
     <div class="submit-wrap">
         <button type="submit" id="submit" class="login-button">
             <span><?php p($l->t('Verify')); ?></span>
