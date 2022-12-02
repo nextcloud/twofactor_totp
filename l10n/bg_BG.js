3dfd9733419891b2b80392e0779342a32cf8829a
ownClouders <devops@owncloud.com>
2022-12-02 01:37:42 +0000

    [tx] updated from transifex

--- l10n/bg_BG.js
+++ l10n/bg_BG.js
@@ -2,7 +2,7 @@ OC.L10N.register(
     "twofactor_totp",
     {
     "Activate TOTP" : "Активиране TOTP",
-    "This is your new TOTP secret:" : "Това е Вашата TOTP парола:",
+    "This is your new TOTP secret:" : "Това е вашата нова TOTP тайна:",
     "Scan this QR code with your TOTP app" : "Сканирайте този QR код с Вашето TOTP приложение",
     "To enable second-factor verify authentication code below." : "За да активирате втори фактор за удостоверяване, кода е по-долу.",
     "Authentication code" : "Удостоверителен код",
@@ -10,7 +10,7 @@ OC.L10N.register(
     "Verified" : "Потвърдено",
     "Not verified" : "Не е потвърдено",
     "Time-based One-time Password" : "Еднократна парола, базирана на време",
-    "Authenticate with a TOTP app" : "Удостоверете с TOTP приложение",
+    "Authenticate with a TOTP app" : "Удостоверяване с приложението TOTP",
     "TOTP Second-factor Auth" : "TOTP Втори-фактор Auth"
 },
 "nplurals=2; plural=(n != 1);");
