8487f40cfae27e46115e6aeae383c88f8945310e
Niklas Seyfarth <niklas@seyfarth.de>
2024-04-21 16:59:19 +0200

    Show warning in initial setup for accounts without email.

--- src/components/LoginSetup.vue
+++ src/components/LoginSetup.vue
@@ -21,7 +21,10 @@
   -->
 
 <template>
-	<div>
+	<div v-if="errorHint">
+		<span class="error"> {{ errorHint }} </span>
+	</div>
+	<div v-else>
 		<div v-if="loading" class="loading" />
 		<SetupConfirmation v-else
 			:loading="confirmationLoading"
@@ -48,6 +51,7 @@ export default {
 			loading: true,
 			confirmationLoading: false,
 			email: '',
+			errorHint: '',
 			confirmation: '',
 		}
 	},
@@ -65,9 +69,22 @@ export default {
 
 					this.email = email
 
+					if (!this.email) {
+						this.errorHint = t('twofactor_email', 'Unable to send email authentication code, because no user email address is set!')
+					}
+
 					this.loading = false
 				},
 			)
+			.catch((e) => {
+				this.errorHint = t('twofactor_email', 'Unable to activate email authentication. It\'s possible that the server is experiencing difficulties with mail delivery.')
+				Logger.error('Could not enable e-mail', e)
+
+				// Restore on error
+				this.loading = false
+			})
+			.catch((e) => Logger.error(e))
+
 		},
 		confirm() {
 			this.loading = true
