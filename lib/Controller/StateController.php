ae84c405c739e0c4b583cf804832592c0ac21297
Olav Seyfarth <olav@seyfarth.de>
2025-10-05 22:40:43 +0200

    Adjust REUSE license header to reflect adopted repository.
    Signed-off-by: Olav Seyfarth <olav@seyfarth.de>

--- lib/Controller/StateController.php
+++ lib/Controller/StateController.php
@@ -3,7 +3,7 @@
 declare(strict_types = 1);
 
 /*
- * SPDX-FileCopyrightText: 2016 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2025 Olav and Niklas Seyfarth, Contributors <https://github.com/datenschutz-individuell/twofactor_email/blob/master/CONTRIBUTORS.md>
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
