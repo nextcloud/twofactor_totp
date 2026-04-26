26e2bba6640730917b269fa038d975138714cefa
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-04-26 04:18:25 +0000

    fix(deps): Fix npm audit
    Signed-off-by: Christoph Wurst <1374172+ChristophWurst@users.noreply.github.com>

---
+++ src/tests/__mocks__/password-confirmation.js
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+module.exports = {
+	confirmPassword: jest.fn(() => Promise.resolve()),
+}
